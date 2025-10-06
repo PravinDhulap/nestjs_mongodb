8c13387c515b574086f7d4a2cbd828337493f900
Pravin Dhulap <dhulap.pravin@gmail.com>
2025-10-06 21:07:28 +0530

    Created createUser function in user service
    Added reference to the User Controller

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,10 +1,41 @@
+import { Model } from 'mongoose';
 import { GetUsersParamDto } from '../dtos/get-users-param.dto';
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { User } from '../user.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { CreateUserDto } from '../dtos/create-user.dto';
 /**
  * Class to connect to Users table and perform business operations
  */
 @Injectable()
 export class UsersService {
+
+  constructor(
+    /**
+     * Injext User Model to perform database operations
+     */
+    @InjectModel(User.name)
+    private readonly userModel: Model<User>,
+
+  ) { }
+
+  public async createUser(createUserDto: CreateUserDto) {
+
+    let newUser = null
+    try {
+      newUser = new this.userModel(createUserDto);
+    } catch (error) {
+      throw new BadRequestException('Error creating the user');
+    }
+    finally {
+      if (!newUser) {
+        return await newUser.save();
+      }
+    }
+
+
+  }
+
   /**
    * The method to get all the users from the database
    */
